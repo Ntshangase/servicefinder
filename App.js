c7a08eee1bc052e7ff4601948850091de37e370d
Ntshangase <simphiwe323i@gmail.com>
2022-09-09 21:49:22 +0200

    splashScreen done

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
